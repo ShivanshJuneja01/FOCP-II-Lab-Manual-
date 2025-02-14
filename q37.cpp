5c334e8ceed3f9db997f27e55592bfaab0c79689
ShivanshJuneja01 <junejashivansh676@gmail.com>
2025-02-14 13:45:09 +0530

    Add files via upload

---
+++ q37.cpp
@@ -0,0 +1,28 @@
+//Input an array of n elements and display it in reverse order. 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    // Taking input for the number of elements
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    // Taking input for the array elements
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Displaying the array in reverse order
+    cout << "Reversed array: ";
+    for (int i = n - 1; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ q40.cpp
@@ -0,0 +1,40 @@
+//Sort an array (logic)
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "The original array is: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    cout << "The sorted array is: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
